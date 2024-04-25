2e05caada670d63c78dfd7805ad6a16a7dbd927e
rurodrig23 <rubennrp37@gmail.com>
2024-04-25 08:20:07 +0200

    comienzo del codigo

---
+++ codificacio.java/media.java
@@ -0,0 +1,23 @@
+package codificacio.java;
+
+public class media {
+
+    class Classe {
+        private String nom;
+        private String autor;
+        private int duradaSegons;
+
+        }
+        public int getNom(){
+            return this.nom;
+        }
+        public int getAutor(){
+            return this.autor;
+        }
+
+        public int getduradaSegons(){
+            return this.duradaSegons;
+        }
+    }
+    
+
