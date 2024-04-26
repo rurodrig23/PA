62a22c2bdb456c6cd549e82dce35b6aee62ff369
rurodrig23 <rubennrp37@gmail.com>
2024-04-26 08:37:53 +0200

    mismo public class para fantasma

--- pacma/pacman.java
+++ pacma/pacman.java
@@ -7,52 +7,57 @@ public class pacman{
 
 
 
-public pacman(int newPosX, int newPosY){
+    public pacman(int newPosX, int newPosY){
 
-    this.newPosX = newPosX;
-    this.newPosX = newPosY;
+        this.newPosX = newPosX;
+        this.newPosX = newPosY;
 }
 
-public void move(int newPosX, int newPosY){
+    public void move(int newPosX, int newPosY){
 
 
 
 }
 
-public void pickupBola(){
+    public void pickupBola(){
 
 
     
 }
 
 
-public void pickupBolaEspecial(){
+    public void pickupBolaEspecial(){
 
 
     
 }
 
-public void loseLife(){
+    public void loseLife(){
 
 
     
 }
 
-public void detectColisionFantasma(){
+    public void detectColisionFantasma(){
 
 
     
 }
+}
+
+
+public class fantasma{
+
+    private int newPosX;
+    private int newPosY;
 
 
 
+    public fantasma(int newPosX, int newPosY){
 
-
-
-
-
-
-
+        this.newPosX = newPosX;
+        this.newPosX = newPosY;
+}
 
 
 }
