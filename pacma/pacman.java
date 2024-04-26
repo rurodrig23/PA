8a04fc3a980999e43dac7a9a57e5e6f2f82abd6c
rurodrig23 <rubennrp37@gmail.com>
2024-04-26 08:33:17 +0200

    public void de todas las funciones

--- pacma/pacman.java
+++ pacma/pacman.java
@@ -5,10 +5,54 @@ public class pacman{
     private int newPosX;
     private int newPosY;
 
-}
+
 
 public pacman(int newPosX, int newPosY){
 
     this.newPosX = newPosX;
     this.newPosX = newPosY;
+}
+
+public void move(){
+
+
+
+}
+
+public void pickupBola(){
+
+
+    
+}
+
+
+public void pickupBolaEspecial(){
+
+
+    
+}
+
+public void loseLife(){
+
+
+    
+}
+
+public void detectColisionFantasma(){
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
 }
