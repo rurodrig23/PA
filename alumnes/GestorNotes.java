449c813c7ae4eed82e40c04161e85b5b9917251b
rurodrig23 <rubennrp37@gmail.com>
2024-05-02 09:07:17 +0200

    entrega

--- alumnes/GestorNotes.java
+++ alumnes/GestorNotes.java
@@ -3,20 +3,25 @@ package alumnes;
 public class GestorNotes{
 
     private double[][] notesAlumnes;
-    private String[] nomAlumne;
+    private String[] nomAlumnes;
     private int count;
 
     public GestorNotes(int numAlumnes, int numAssignatures)
 
         notesAlumnes= new double[numAlumnes][numAssignatures];
-        nomAlumne = new String [numAlumnes];
-        count=0
+        nomAlumnes = new String [numAlumnes];
+        count=0;
 }
-  public void resgistarNotes ()
-        int i = 0
-        while (i < notesAlumnes.lenght && nomAlumne[i]){
+    public void resgistarNotes(String nomAlumne, double[] notes) {
+        nomAlumnes[count] = nomAlumne;
+        notesAlumnes[count] = notes;
+        count++;
 
-        i++;
+    public double[] obtenirNotes(){
 
-        }
+      
+
+    }
+
+  }
 
