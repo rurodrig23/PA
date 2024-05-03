b65df2c6119efc2dd81878773cb5e0403e0e8f1c
rurodrig23 <rubennrp37@gmail.com>
2024-05-03 08:47:18 +0200

    empezamos el main importando los datos de arbreBinari el cual no me pasa nada, y creamos dos arboles entreros

---
+++ Implementacion.java/App/Main.java
@@ -0,0 +1,12 @@
+package App;
+import trees.arbreBinari;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        arbreBinari intArbol1 = new arbreBinari();
+        arbreBinari intArbol2 = new arbreBinari();
+        
+    }
+}
