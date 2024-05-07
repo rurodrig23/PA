d2192080e46e0e84d025e03cc54071fc8594b406
rurodrig23 <rubennrp37@gmail.com>
2024-05-07 10:59:59 +0200

    gestor tasques

---
+++ gestorTasques.java/GestorTasques.java
@@ -0,0 +1,14 @@
+
+package gestorTasques.java;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+
+public class GestorTasques {
+    
+    private ArrayList<Tasca> tasques;
+    private HasMap<int, Tasca> tasquesCompletades;
+}
