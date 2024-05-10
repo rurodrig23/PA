13e464d46fd9af9ab9d71eeae7bb66d66f0a8866
rurodrig23 <rubennrp37@gmail.com>
2024-05-10 08:48:27 +0200

    set y getters

--- GestioAventura/Personaje.java
+++ GestioAventura/Personaje.java
@@ -3,7 +3,7 @@ import javax.xml.parsers.*;
 import org.w3c.dom.*;
 import java.io.*;
 
-public class Personaje {
+public class Personaje<T> {
     private String nombre;
     private int nivel;
     private int puntosDeVida;
@@ -23,4 +23,55 @@ public class Personaje {
     this.armadura = armadura;
     }
 
+
+
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public int getNivel(){
+        return nivel;
+
+    }
+    public void setNivel(int nivel){
+        this.nivel = nivel;
+        }    
+
+    public int getPuntosDeVida(){
+        return puntosDeVida;
+
+    }
+
+    public void setPuntosDeVida(int puntosDeVida) {
+        this.puntosDeVida = puntosDeVida;
+    }
+
+    public int getPuntosDeMana(){
+        return puntosDeMana;
+
+    }
+
+    public void setPuntosDeMana(int puntosDeMana) {
+        this.puntosDeMana = puntosDeMana;
+    }
+
+    public String getArma() {
+        return arma;
+    }
+
+    public void setArma(String arma) {
+        this.arma = arma;
+    }
+    public String getArmadura() {
+        return armadura;
+    }
+
+    public void setArmadura(String armadura) {
+        this.armadura = armadura;
+    }
 }
