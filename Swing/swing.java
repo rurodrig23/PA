e3afafdf3c5061e9f3db10491dc880948502b601
rurodrig23 <rubennrp37@gmail.com>
2024-05-13 13:34:16 +0200

    mañana mas

--- Swing/swing.java
+++ Swing/swing.java
@@ -1,16 +1,31 @@
 package Swing;
 import javax.swing.*;
+import javax.swing.undo.StateEditable;
+
+import java.awt.event.*;
+
 
 class swing {
     
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Finestra d'exemple");
-        JButton button = new JButton("Clic aqui");
-        
-        frame.add(button);
-        frame.setSize(300,200);
+        JFrame frame = new JFrame("Calculadora");
+        JTextField pantalla = new JTextField();
+        JButton[] numericButtons = new JButton[10];
+
+        for (int i = 0; i < numericButtons.length; i++){
+            numericButtons[i] = new JButton(Integer.toString(i));
+
+        }
+
+        pantalla.setEditable(false);
+
+        frame.add(pantalla);
+        for (JButton jButton : numericButtons) {
+            frame.add(jButton);
+            
+        }
+        frame.setSize(200, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
-
 }
