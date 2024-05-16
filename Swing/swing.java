06dd04d00df0aa6a702617ad524fecba8145a415
rurodrig23 <rubennrp37@gmail.com>
2024-05-16 09:31:38 +0200

    solucion

--- Swing/swing.java
+++ Swing/swing.java
@@ -2,7 +2,8 @@ package Swing;
 import javax.swing.*;
 import javax.swing.undo.StateEditable;
 
-import java.awt.event.*;
+import java.awt.GridLayout;
+import java.awt.*;
 
 
 class swing {
@@ -11,21 +12,28 @@ class swing {
         JFrame frame = new JFrame("Calculadora");
         JTextField pantalla = new JTextField();
         JButton[] numericButtons = new JButton[10];
+        JPanel buttonsPanel = new JPanel();
+        GridLayout managerButtons = new GridLayout(4, 3);
+        BoxLayout managerGeneral = new BoxLayout(frame, BoxLayout.Y_AXIS);
+       
+        buttonsPanel.setLayout(managerButtons);
 
         for (int i = 0; i < numericButtons.length; i++){
             numericButtons[i] = new JButton(Integer.toString(i));
 
         }
-
         pantalla.setEditable(false);
 
-        frame.add(pantalla);
         for (JButton jButton : numericButtons) {
-            frame.add(jButton);
+            buttonsPanel.add(jButton);
             
         }
+        frame.add(buttonsPanel);
+        frame.setLayout(managerButtons);
+        frame.add(pantalla);
         frame.setSize(200, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
-}
+
+    }
