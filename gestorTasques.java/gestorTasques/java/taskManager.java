2ecbc40a5ed8618531bd8a5564fc17728f26418f
rurodrig23 <rubennrp37@gmail.com>
2024-05-17 09:20:01 +0200

    funciones para guardar tasca y cargarla, new file implementado vamos a por el main y ver que errores tengo

--- gestorTasques.java/gestorTasques/java/taskManager.java
+++ gestorTasques.java/gestorTasques/java/taskManager.java
@@ -1,11 +1,15 @@
 package gestorTasques.java;
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
 
 class TaskManagement
 {
+	File file = new File("/home/alumne_1r/Escriptori/PA/gestorTasques.java/archivo.txt");
 	HashMap<String, ArrayList<Task>> lists;
 	HashMap<Integer, Task> tasks;
 	String fileName;
@@ -17,19 +21,21 @@ class TaskManagement
 		lists.put("En curso", new ArrayList<Task>());
 		lists.put("Completada", new ArrayList<Task>());
 		tasks = new HashMap<Integer, Task>();
-		
+		loadTasksFromFile();
 	}
 
 	public void addTask(String title, String description, String dueDate, String status) throws ParseException {
         Task task = new Task(title, description, dueDate, status);
         lists.get(status).add(task);
         tasks.put(task.getId(), task);
+		saveTasksToFile();
         
     }
 
 	public void doneTask(int taskId) {
         Task task = tasks.get(taskId);
         task.setStatus("Completada");
+		saveTasksToFile();
     }
 
 	public void deleteTask(int taskId) {
@@ -51,4 +57,36 @@ class TaskManagement
 			System.out.println(task);
 		}
 	}
-}
+
+	private void loadTasksFromFile() {
+        try {
+            File file = new File(fileName);
+            if (file.exists()) {
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] fields = line.split(",");
+				}
+				reader.close();
+
+				}
+			} catch (IOException | ParseException e) {
+				System.out.println("Error loading tasks from file: " + e.getMessage());
+			}
+			}
+
+
+			private void saveTasksToFile() {
+				try {
+					BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+					for (Task task : tasks.values()) {
+						writer.write(task.toString());
+						writer.write("\n");
+					}
+					writer.close();
+				} catch (IOException e) {
+					System.out.println("Error saving tasks to file: " + e.getMessage());
+				}
+		}
+	}
+	
