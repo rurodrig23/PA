8ef0cbc54588b39faf2f9baaeb86a5112b94cbbe
rurodrig23 <rubennrp37@gmail.com>
2024-05-17 09:54:05 +0200

    pruebas

--- gestorTasques/task.java
+++ gestorTasques/task.java
@@ -13,12 +13,12 @@ class Task
     private Date dueDate;
     private String status;
 
-	public Task(String title, String description, java.sql.Date date,String status) throws ParseException {
-		this.title = title;
+	public Task(String title, String description, String dueDate, String status) throws ParseException {
+        this.title = title;
         this.description = description;
-        this.dueDate = new SimpleDateFormat("dd/MM/yyyy").parse(date);
+        this.dueDate = new SimpleDateFormat("dd/MM/yyyy").parse(dueDate);
         this.status = status;
-	}
+    }
 
     public int getId() {
         return id;
