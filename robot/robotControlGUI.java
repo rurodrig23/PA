b5ef64160f707342891480b75199a232198d9c43
rurodrig23 <rubennrp37@gmail.com>
2024-05-24 09:14:10 +0200

    p

--- robot/robotControlGUI.java
+++ robot/robotControlGUI.java
@@ -2,15 +2,16 @@ package robot;
 import javax.swing.*;
 import javax.swing.undo.StateEditable;
 
-import java.awt.GridLayout;
 import java.awt.*;
 
 
 public class robotControlGUI {
+    private JTextField pantalla;
+
      public static void main(String[] args) {
         JFrame frame = new JFrame("ROBOT");
         JTextField pantalla = new JTextField();
-        JPanel buttonsPanel = new JPanel(new GridLayout(3, 2));
+        JPanel buttonsPanel = new JPanel();
 
 
         JButton endavant = new JButton("Moure Endavant");
@@ -21,8 +22,7 @@ public class robotControlGUI {
         JButton carregar = new JButton("Carregar Estat");
 
         
-        buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.Y_AXIS));
-
+        buttonsPanel.setLayout(new GridLayaout(3, 2));
         buttonsPanel.add(endavant);
         buttonsPanel.add(enrere);
         buttonsPanel.add(esquerra);
@@ -30,11 +30,10 @@ public class robotControlGUI {
         buttonsPanel.add(guardar);
         buttonsPanel.add(carregar);
 
-        frame.setLayout(new GridLayout(3, 2));
-        frame.add(buttonsPanel);
-        frame.add(pantalla);
-      
-
+        frame.setLayout(new BorderLayout());
+        frame.add(buttonsPanel, BorderLayout.NORTH);
+        frame.add(pantalla, BorderLayout.CENTER);
+    
         frame.setSize(375, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
