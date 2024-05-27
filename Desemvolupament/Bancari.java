141d5b629403370bced6e60b98e15aef53da7031
rurodrig23 <rubennrp37@gmail.com>
2024-05-27 08:50:15 +0200

    funciones reritr y depositar

--- Desemvolupament/Bancari.java
+++ Desemvolupament/Bancari.java
@@ -39,4 +39,31 @@ public void setSaldo(double saldo) {
 
     this.saldo = saldo;
 }
+public void depositar(double cantidad) {
+
+    if (cantidad > 0) {
+
+        saldo += cantidad;
+
+    }
+
+}
+
+
+public boolean retirar(double cantidad) {
+
+    if (cantidad > 0 && saldo >= cantidad) {
+
+        saldo -= cantidad;
+
+        return true;
+
+    } else {
+
+        return false;
+
+    }
+
+}
+
 }
