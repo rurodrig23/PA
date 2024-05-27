aa4d0ad1b44130bde834b311c59d8ffa2a6bb866
rurodrig23 <rubennrp37@gmail.com>
2024-05-27 08:09:53 +0200

    public class

---
+++ Desemvolupament/GestorLlibres.java
@@ -0,0 +1,10 @@
+package Desemvolupament;
+
+import java.util.HashMap;
+
+public class GestorLlibres {
+    private HashMap<Integer, Llibre> llibreria;
+
+    public GestorLlibres() {
+        this.llibreria = new HashMap<>();
+    }
