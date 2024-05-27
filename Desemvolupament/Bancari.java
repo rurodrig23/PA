94540a7a36cfbdf569cdb6c07438b1bb4a48b8a3
rurodrig23 <rubennrp37@gmail.com>
2024-05-27 08:47:05 +0200

    public class y comienzo

---
+++ Desemvolupament/Bancari.java
@@ -0,0 +1,16 @@
+package Desemvolupament;
+
+public class Bancari {
+
+    private String numeroDeCuenta;
+
+    private double saldo;
+
+
+    public Bancari(String numeroDeCuenta, double saldo) {
+
+        this.numeroDeCuenta = numeroDeCuenta;
+
+        this.saldo = saldo;
+    }
+}
