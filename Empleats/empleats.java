f650eab76d274acdaa0c3ea977f1b842dda2784e
rurodrig23 <rubennrp37@gmail.com>
2024-05-27 09:04:12 +0200

    public class

---
+++ Empleats/empleats.java
@@ -0,0 +1,11 @@
+package Empleats;
+
+public class empleats {
+    protected String nom;
+    protected double salari;
+
+    public empleats(String nom, double salari) {
+        this.nom = nom;
+        this.salari = salari;
+    }
+}
