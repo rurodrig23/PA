eea9657d06a6018d35891933597dc3e02c789dee
rurodrig23 <rubennrp37@gmail.com>
2024-05-27 08:16:05 +0200

    añadimos class llibre y sus funciones, ahora vamos a implementar esas funciones

--- Desemvolupament/GestorLlibres.java
+++ Desemvolupament/GestorLlibres.java
@@ -8,3 +8,36 @@ public class GestorLlibres {
     public GestorLlibres() {
         this.llibreria = new HashMap<>();
     }
+
+class Llibre {
+    private String titol;
+    private String autor;
+    private boolean prestat;
+    private String usuario;
+
+    public Llibre(String titol, String autor) {
+        this.titol = titol;
+        this.autor = autor;
+        this.prestat = false;
+    }
+
+    public void prestar(String usuario) {
+        this.prestat = true;
+        this.usuario = usuario;
+    }
+
+
+public void tornar() {
+    this.prestat = false;
+    this.usuario = null;
+}
+
+public boolean disponible() {
+    return !this.prestat;
+}
+
+public String getUsuari() {
+    return this.usuario;
+}
+}
+}
