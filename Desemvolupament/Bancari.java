0ffd1f32335c6e091a78a7ee20458625ff50ca57
rurodrig23 <rubennrp37@gmail.com>
2024-05-27 08:48:54 +0200

    set y get de numcuenta y saldo

--- Desemvolupament/Bancari.java
+++ Desemvolupament/Bancari.java
@@ -13,4 +13,30 @@ public class Bancari {
 
         this.saldo = saldo;
     }
+
+public String getNumeroDeCuenta() {
+
+    return numeroDeCuenta;
+
+}
+
+
+public void setNumeroDeCuenta(String numeroDeCuenta) {
+
+    this.numeroDeCuenta = numeroDeCuenta;
+
+}
+
+
+public double getSaldo() {
+
+    return saldo;
+
+}
+
+
+public void setSaldo(double saldo) {
+
+    this.saldo = saldo;
+}
 }
