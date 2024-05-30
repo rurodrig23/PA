25d40a896fc952eec4de790a0c1ea90e4e8f0f86
rurodrig23 <rubennrp37@gmail.com>
2024-05-30 13:32:16 +0200

    final

--- GestioCine/cine.java
+++ GestioCine/cine.java
@@ -2,7 +2,7 @@ package GestioCine;
 import java.sql.*;
 
 
-class GestionBibloteca {
+class GestioCine {
     public static Connection getConnection() {
         String url = "jdbc:mysql://192.168.56.101:3306/cine";
         String user = "root";
